d7ede4fa8e1508693a23b366745cca4adf1d010c
Joe Ryan <joeryansaccount@googlemail.com>
2018-01-27 20:11:43 +0000

    onClickDealButton method written - when click dealer and player receive card each

--- android_project/cardgame/BeatTheDealer/src/main/java/com/example/josephryan/cardgame/Android/GameActivity.java
+++ android_project/cardgame/BeatTheDealer/src/main/java/com/example/josephryan/cardgame/Android/GameActivity.java
@@ -3,6 +3,8 @@ package com.example.josephryan.cardgame.Android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.josephryan.cardgame.Java.Dealer;
 import com.example.josephryan.cardgame.Java.DeckOfCards.Deck;
@@ -14,6 +16,7 @@ public class GameActivity extends AppCompatActivity {
     Deck deck;
     Dealer dealer;
     Player player1;
+    Button dealButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +26,22 @@ public class GameActivity extends AppCompatActivity {
         deck = new Deck();
         dealer = new Dealer(deck);
         player1 = new Player();
-
         dealer.addPlayer(player1);
+        dealButton = findViewById(R.id.deal);
 
         String checkPlayersInGame = Integer.toString(dealer.getPlayerCount());
         Log.d("number of players: ", checkPlayersInGame);
         String hasDeck = Integer.toString(dealer.getDeck().getCardDeck().size());
         Log.d("full deck: ", hasDeck);
+    }
+
+    public void onDealButtonClick(View button){
+        dealer.dealForRound();
+
+        String playerHasCards = Integer.toString(player1.getHand().size());
+        Log.d("Player number of cards:", playerHasCards);
+        String dealerHasCards = Integer.toString(dealer.getHand().size());
+        Log.d("Dealer number of cards:", dealerHasCards);
     }
 
 
